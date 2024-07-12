c63128caa86eec063a034c123859338b82f4897e
stonoy <sowrabh2013@gmail.com>
2024-07-12 17:11:50 +0530

    cd

--- scripts/buildprod.sh
+++ scripts/buildprod.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
+# Navigate to the client directory
+cd client
+
+# Run npm build
+npm run build
+
+# Check if npm build was successful
+if [ $? -ne 0 ]; then
+  echo "npm build failed"
+  exit 1
+fi
+
+# Return to the root directory
+cd ..
+
+# Build the Go application
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o exam1
+
+# Check if Go build was successful
+if [ $? -ne 0 ]; then
+  echo "Go build failed"
+  exit 1
+fi
+
+echo "Build successful"
